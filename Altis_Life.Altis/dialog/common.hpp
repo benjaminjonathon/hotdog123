1977bb1f8166f0b08726da461cba9ddc1a881768
Liquid-Omega <caliguirida@upmc.edu>
2016-01-24 20:25:52 -0500

    Button fix

--- Altis_Life.Altis/dialog/common.hpp
+++ Altis_Life.Altis/dialog/common.hpp
@@ -177,20 +177,7 @@ class Life_RscText {
 	tooltipColorShade[] = {0,0,0,0.65};
 };
 
-class TextMsgButton : Life_RscButtonMenu {
-	animTextureNormal = "#(argb,8,8,3)color(1,1,1,0)";
-	animTextureDisabled = "#(argb,8,8,3)color(1,1,1,0)";
-	animTextureOver = "#(argb,8,8,3)color(1,1,1,0)";
-	animTextureFocused = "#(argb,8,8,3)color(1,1,1,0)";
-	animTexturePressed = "#(argb,8,8,3)color(1,1,1,0)";
-	animTextureDefault = "#(argb,8,8,3)color(1,1,1,0)";
-	colorBackground[] = {0, 0, 0, 0};
-	colorBackground2[] = {1, 1, 1, 0};
-	color[] = {1, 1, 1, 0};
-	color2[] = {1, 1, 1, 0};
-	colorText[] = {1, 1, 1, 0};
-	colorDisabled[] = {1, 1, 1, 0};
-};
+
 
 class Life_RscLine : Life_RscText {
 	idc = -1;
@@ -521,6 +508,21 @@ class Life_RscShortcutButtonMain : Life_RscShortcutButton {
 		color = "#E5E5E5";
 		align = "false";
 	};
+};
+
+class Life_RscButtonInvisible : Life_RscButtonMenu {
+	animTextureNormal = "#(argb,8,8,3)color(1,1,1,0)";
+	animTextureDisabled = "#(argb,8,8,3)color(1,1,1,0)";
+	animTextureOver = "#(argb,8,8,3)color(1,1,1,0)";
+	animTextureFocused = "#(argb,8,8,3)color(1,1,1,0)";
+	animTexturePressed = "#(argb,8,8,3)color(1,1,1,0)";
+	animTextureDefault = "#(argb,8,8,3)color(1,1,1,0)";
+	colorBackground[] = {0, 0, 0, 0};
+	colorBackground2[] = {1, 1, 1, 0};
+	color[] = {1, 1, 1, 0};
+	color2[] = {1, 1, 1, 0};
+	colorText[] = {1, 1, 1, 0};
+	colorDisabled[] = {1, 1, 1, 0};
 };
 
 class Life_RscCheckbox {
